ca5acc8e206adb0159410553d060a1b3db1c6a9e
Yuxuan (Tim) Zhang <timzhangyuxuan@gmail.com>
2016-10-25 16:14:54 -0500

    Update Expanded_NN.m

--- Enhanced Neural Network/Expanded_NN.m
+++ Enhanced Neural Network/Expanded_NN.m
@@ -11,7 +11,7 @@ function [weights, biases] = Expanded_NN(input,target,split,nodeLayers,numEpochs
     validation_label = target(:,index_valid);
     test_label = target(:,index_test);
 
-    % initiate lists for plotting
+    % initiate lists for performance plotting
     epoch_list = []; 
     train_cost_list = [];
     valid_cost_list = [];
@@ -25,19 +25,17 @@ function [weights, biases] = Expanded_NN(input,target,split,nodeLayers,numEpochs
     % placeholder for biases between different layers
     biases = cell(1,length(nodeLayers)-1);
 
-    % placeholder for velocities of all weights and biases
+    % placeholder for velocities of all weights and biases, will be used for momentum parameter updates
     velocity_w = cell(1,length(nodeLayers)-1);
     velocity_b = cell(1,length(nodeLayers)-1);
 
     % placeholder for weighted sum
     % the first elem is weighted sum of input nodes and first layer weights
     z = cell(1,length(nodeLayers)-1);
-    %z_perf = cell(1,length(nodeLayers)-1); % for performance use
 
     % placeholder for activation nodes
     % the first elem is the input layer
     a = cell(1,length(nodeLayers));
-    %a_perf = cell(1,length(nodeLayers)); % for performance use
 
     % placeholder for gradient of loss w.r.t. activation function for all
     % layers except first layer
@@ -82,12 +80,14 @@ function [weights, biases] = Expanded_NN(input,target,split,nodeLayers,numEpochs
             mini_train = minibatch(1:nodeLayers(1),:);
             mini_label = minibatch(nodeLayers(1)+1:end,:);
 
-            % the first elem are input nodes
+            % the first elem of a is input nodes, which are mini-batch training examples
             a{1} = mini_train; 
 
             % feed-forward
             for conn=1:length(nodeLayers)-1 % for each connections between layers
-                z{conn} = weights{conn}*a{conn} + biases{conn}; % weighted sum
+                z{conn} = weights{conn}*a{conn} + biases{conn}; % weighted sum of nodes from previous layer and weights
+                
+                % calculate activations of the next layer
                 if isequal(trans, 'sigmoid')
                      a{conn+1} = logsig(z{conn}); % sigmoid transformation of z 
                 elseif isequal(trans, 'tanh')
@@ -113,10 +113,8 @@ function [weights, biases] = Expanded_NN(input,target,split,nodeLayers,numEpochs
             end
 
             % back-propagation  
-            % gradient of loss (delta) w.r.t. activation function in layers
-            % except the first layer.
-            % If we have 3-layered network, then we compute delta for last two
-            % layers
+            % gradient of loss (delta) w.r.t. activation function in layers except the first layer.
+            % If we have 3-layered network, then we compute delta for last two layers
             if isequal(cost, 'quadratic')
                 delta_layers{length(nodeLayers)} = (a{end}-mini_label) .* delta_activation(trans, a{end}, 'is_output_layer'); % delta for output nodes
                 for layer=length(nodeLayers)-1:-1:2 % going backwards
@@ -134,8 +132,7 @@ function [weights, biases] = Expanded_NN(input,target,split,nodeLayers,numEpochs
                 end
             end
 
-            % compute gradient of loss w.r.t. parameters, including L2
-            % regularization
+            % compute gradient of loss w.r.t. parameters, including L2 regularization
             for layer=length(nodeLayers)-1:-1:1 
                 delta_weights{layer} = delta_layers{layer+1} * a{layer}' + lambda * weights{layer};
                 temp_b = sum(delta_layers{layer+1},2);
@@ -144,7 +141,6 @@ function [weights, biases] = Expanded_NN(input,target,split,nodeLayers,numEpochs
 
             % update weights and biases using momentum
             for layer=length(nodeLayers)-1:-1:1 
-
                velocity_w{layer} = mu * velocity_w{layer} - ((eta/size(mini_train,2)) .* delta_weights{layer});
                weights{layer} = weights{layer} + velocity_w{layer};
 
@@ -153,9 +149,11 @@ function [weights, biases] = Expanded_NN(input,target,split,nodeLayers,numEpochs
             end
         end
 
+        % get performance on training, validation and test sets
         [train_cost,train_num_correct,train_accu] = perfmance(train,train_label,nodeLayers,weights,biases,trans,cost,lambda);
         [valid_cost,valid_num_correct,valid_accu] = perfmance(validation,validation_label,nodeLayers,weights,biases,trans,cost,lambda);
         [test_cost,test_num_correct,test_accu] = perfmance(test,test_label,nodeLayers,weights,biases,trans,cost,lambda);
+        
         % report learning status for each epoch
         epoch_list = [epoch_list, epoch]; % append epoch
         train_cost_list = [train_cost_list,train_cost];
@@ -169,12 +167,13 @@ function [weights, biases] = Expanded_NN(input,target,split,nodeLayers,numEpochs
                 test_cost,test_num_correct,size(test,2),test_accu);
         end
 
-        % early stopping
+        % early stopping criterion
         if valid_accu > 0.99
            break; 
         end
     end
 
+    % plot prediction cost for training, validation and testing sets
     figure,
     plot(epoch_list,train_cost_list,'DisplayName','Train')
     hold on
